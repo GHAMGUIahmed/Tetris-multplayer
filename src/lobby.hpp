7926f0824ad79331cc9bc3d47d93ab3bd2feb0e8
Elyes Hajji <116601633+Elyes420@users.noreply.github.com>
2024-03-18 13:06:06 +0100

    Update lobby.hpp

--- src/lobby.hpp
+++ src/lobby.hpp
@@ -25,39 +25,39 @@ class Lobby
         Nothing
     };
 
-    State state;
-    sf::Font lobbyFont;
-    sf::Text lobbyText;
-    sf::Texture texture;
+    State state;        //État actuel du lobby
+    sf::Font lobbyFont;        // Police de caractères pour le lobby
+    sf::Text lobbyText;        // Texte utilisé dans le lobby
+    sf::Texture texture;       // Texture utilisée dans le lobby 
 
-    Server* server;
-    Client* client;
+    Server* server;            // Pointeur vers l'instance du serveur 
+    Client* client;            // Pointeur vers l'instance du client
 
-    Tetris* game;
+    Tetris* game;               // Pointeur vers l'instance du jeu Tetris
 
-    bool gameStarted = false;
-    Typing typing = Nothing;
-    std::string typeText = "";
-    std::string address = "127.0.0.1";
+    bool gameStarted = false;        //Indique si le jeu a commencé
+    Typing typing = Nothing;         // État de la saisie
+    std::string typeText = "";       //Texte en cours de saisie
+    std::string address = "127.0.0.1";        //Adresse IP par défaut (localhost)
 
-    std::string options[3] =
+    std::string options[3] =           //Options du jeu 
     {
     "Singleplayer",
     "Join game",
     "Host game"
     };
 
-    int selected = 0;
+    int selected = 0;            //Indice de l'option sélectionnée
 
 public:
-    static bool KEYBOARD_FOCUS;
+    static bool KEYBOARD_FOCUS;        //Indique si le focus clavier est sur la fenêtre du lobby
 
 
     Lobby();
-    void update();
-    void draw(sf::RenderWindow& window);
-    void text_input(char c);
-    void drawUserWorlds(sf::RenderWindow& window);
+    void update();        //Met à jour l'état du lobby
+    void draw(sf::RenderWindow& window);        //Dessine le contenu du lobby sur la fenêtre
+    void text_input(char c);            //Gère la saisie de texte dans le lobby
+    void drawUserWorlds(sf::RenderWindow& window);    //Dessine les grilles des joueurs adverses sur la fenêtre
 
 
 };
