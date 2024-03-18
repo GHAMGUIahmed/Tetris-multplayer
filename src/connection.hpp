03d24ddb8e645198c1fbc090cda08d9acb957f17
GHAMGUIahmed <ghamguiahmed32@gmail.com>
2024-03-18 15:56:38 +0100

    ##

--- src/connection.hpp
+++ src/connection.hpp
@@ -8,17 +8,17 @@
 class Connection {
 
 private:
-    Server* server;    //Pointeur vers l'instance du serveur auquel la connexion est associée
-    sf::TcpSocket* socket;    //Pointeur vers le socket TCP associé à la connexion 
-    std::string username;      //Username de la connexion     
-    int id;             //ID de la connexion
-    bool running;       //Indique si la connexion est en cours d'exécution
+    Server* server;
+    sf::TcpSocket* socket;
+    std::string username;
+    int id;
+    bool running;
 public:
     Connection(int id, Server* server, sf::TcpSocket* socket);
-    void send(sf::Packet packet);  //Envoie un paquet via la connexion
-    void run();         //Fonction de traitement principal de la connexion 
-    void stop();        //Arrêt de la connexion
-    std::string getName();      //Retourne le username de la connexion
+    void send(sf::Packet packet);
+    void run();
+    void stop();
+    std::string getName();
 };
 
 #endif //CONNECTION_H
